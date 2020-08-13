a74b5559efb216c14d32027b46f0e9c46582284b
k0sior <58810214+k0sior@users.noreply.github.com>
2020-08-13 12:48:17 +0200

    Add files via upload

---
+++ 100-doors.js
@@ -0,0 +1,32 @@
+function getFinalOpenedDoors(numDoors) {
+  // Good luck!
+  let doors = []
+
+  for (let i = 0; i <= 100; i++) { 
+    doors.push("closed")
+  }
+
+  let test = [...Array(numDoors + 1).keys()];
+
+  for (let j = 1; j < test.length; j++) {
+    test.forEach(function(doorNum){
+      if (doorNum % j === 0) {
+        if (doors[doorNum] === "closed") {
+          doors[doorNum] = "open"
+        } else if (doors[doorNum] === "open") {
+          doors[doorNum] = "closed"
+      }}
+    })
+  }
+
+  let result = [];
+  for (let i = 0; i < doors.length; i++) {
+    if (doors[i] === "open") {
+      result.push(i)
+    }
+  };
+
+  return result;
+}
+
+console.log(getFinalOpenedDoors(100))
---
+++ pairwise.js
@@ -0,0 +1,24 @@
+function pairwise(arr, arg) {
+ // arr[i] + arr[j] === arg
+  let result = [];
+  
+  for(let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j){
+        //do nothing
+      } else if (arr[i] + arr[j] == arg) {
+          result.push(i)  // sol. 2: let sum = sum + i + j
+          result.push(j)
+          arr.splice(i, 1, NaN);
+          arr[j] = NaN;
+      }
+    }
+  }
+  result.push(0) //to return a number if (arr === [])
+
+  var sum = result.reduce((a, b) => a + b); //sum of all indicies
+
+  return sum;
+}
+
+
